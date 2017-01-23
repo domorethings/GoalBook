74b42dae4886ed80b74d3aebf0792cb8756054f7
Eric Yang <ericliuyang@gmail.com>
2017-01-23 12:32:55 -0800

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  GoalBook
+//
+//  Created by Eric Yang on 1/21/17.
+//  Copyright © 2017 Eric Liu Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    //MARK: Properties
+    @IBOutlet weak var goalNameTextField: UITextField!
+    @IBOutlet weak var todaysGoalsLabel: UILabel!
+    
+    //MARK: Actions
+    @IBAction func setDefaultTextField(sender: AnyObject) {
+        
+        //Changes the label after the button is pressed
+        todaysGoalsLabel.text = " New Goal Is Set!"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Handles the text field's user input through delegate callback
+        goalNameTextField.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
